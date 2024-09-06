71bf69824533f236de4c99bad5e15b24b15093e2
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-05 23:04:40 -0400

    Creación de la Clase TiposTecnicos en la Carpeta Models

---
+++ Models/TiposTecnicos.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RegistroTecnicos.Models
+{
+    public class TiposTecnicos
+    {
+        [Key]
+        // Tecnico (ID, Nombre, SueldoHora)
+
+        public int TecnicoId { get; set; }
+
+        [Required(ErrorMessage = "Favor, Ingresar el Nombre del Técnico")]
+        public string NombresTecnico { get; set; }
+
+        [Required(ErrorMessage = "Favor, Ingresar la descripcion del Técnico")]
+        public string Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Favor, Ingrese el valor del sueldo por hora")]
+        public float SueldoHora { get; set; }
+    }
+}
