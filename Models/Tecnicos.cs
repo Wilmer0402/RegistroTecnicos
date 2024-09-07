b22abaac212080bda96cb3bedde18100c60878f6
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-07 16:36:46 -0400

    Modificación en el Tecnicos.css, se agregó la validación para que no se guarde un sueldo por hora en cero.

--- Models/Tecnicos.cs
+++ Models/Tecnicos.cs
@@ -1,22 +1,30 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace RegistroTecnicos.Models
 {
-    public class Tecnicos
-    {
-        [Key]
-        // Tecnico (ID, Nombre, SueldoHora)
+	public class Tecnicos : IValidatableObject
+	{
+		[Key]
+		public int TecnicoId { get; set; }
 
-        public int TecnicoId { get; set; }
+		[Required(ErrorMessage = "Favor, Ingresar el Nombre del Tecnico")]
+		public string NombresTecnico { get; set; }
 
-        [Required(ErrorMessage = "Favor, Ingresar el Nombre del Tecnico")]
-        public string NombresTecnico { get; set; }
+		[Required(ErrorMessage = "Favor, Ingresar la descripcion del Técnico")]
+		public string Descripcion { get; set; }
 
-        [Required(ErrorMessage = "Favor, Ingresar la descripcion del Técnico")]
-        public string Descripcion { get; set; }
+		[Required(ErrorMessage = "Favor, Ingrese el valor del sueldo por hora")]
+		public float SueldoHora { get; set; }
 
-        [Required(ErrorMessage = "Favor, Ingrese el valor del sueldo por hora")]
-        public float SueldoHora { get; set; }
-    }
+		public IEnumerable<ValidationResult> Validate(ValidationContext validationContext)
+		{
+			if (SueldoHora <= 0)
+			{
+				yield return new ValidationResult(
+					"El Sueldo por Hora debe ser mayor que 0",
+					new[] { nameof(SueldoHora) });
+			}
+		}
+	}
 }
-
