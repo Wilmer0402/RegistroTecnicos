68769fb3295e20f59c25de60b7c1dabfca9f8b27
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-13 03:20:07 -0400

    Creación del Models Clientes

---
+++ Models/Clientes.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+namespace RegistroTecnicos.Models
+{
+    public class Clientes
+    {
+        [Key]
+
+        // ClienteId, Nombres, WhatsApp
+
+        public int ClienteId { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Nombre del Cliente")]
+            public string Nombres { get; set; }
+
+        [Required (ErrorMessage = "Favor Ingrese el Numero de WhatsApp del Cliente")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten Numeros")]
+
+        public string WhatsApp { get; set; }
+    }
+}
