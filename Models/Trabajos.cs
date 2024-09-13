dd16d6c2aebefaca7b2d7bba39276df6ee429b72
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-13 03:21:10 -0400

    Creación del Models Trabajos

---
+++ Models/Trabajos.cs
@@ -0,0 +1,41 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RegistroTecnicos.Models
+{
+    public class Trabajos
+    {
+        [Key]
+
+        //TrabajoId, Fecha, ClienteId, TecnicoId, Descripcion, Monto
+
+        public int TrabajoId { get; set; }
+        [Required(ErrorMessage = "Favor Complete este Campo")]
+
+        public DateTime Fecha { get; set; } = DateTime.Now;
+
+        [Required(ErrorMessage = "Favor Ingrese la Descripcion del Trabajo")]
+        [RegularExpression(@"[a-zA-Z\s]+$", ErrorMessage = "Solo se permiten Letras")]
+
+        public string? Descripcion { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingrese el Monto del Trabajo")]
+        [RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten Numeros")]
+
+        public double Monto { get; set; }
+
+        [ForeignKey("Clientes")]
+
+        public int ClienteId { get; set; }
+
+        public Clientes Clientes { get; set; }
+
+        [ForeignKey("Tecnicos")]
+
+        public int TecnicoId {  get; set; }
+
+        public Tecnicos Tecnicos { get; set; }
+
+
+
+    }
+}
