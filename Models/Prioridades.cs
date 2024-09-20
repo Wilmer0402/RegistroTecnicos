7c099fbe66e800959327d8926e37f8eb53fef2aa
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-09-19 21:26:21 -0400

    Creación de Prioridades en el Models

---
+++ Models/Prioridades.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+namespace RegistroTecnicos.Models
+{
+	public class Prioridades
+	{
+		[Key]
+
+		//PrioridadId, Descripcion, Tiempo
+		
+		public int PrioridadId { get; set; }
+		
+		[Required(ErrorMessage = "Favor Ingrese la Descripcion de la Prioridad del Trabajo")]
+		[RegularExpression(@"[a-zA-Z\s]+$", ErrorMessage ="Solo se permiten letras")]
+
+		public string? Descripcion { get; set; }
+
+		[Required(ErrorMessage = "Favor Ingrese el Tiempo")]
+		[RegularExpression(@"^\d+$", ErrorMessage = "Solo se permiten Numeros")]
+
+		public int Tiempo { get; set; }
+	}
+}
