82e80d50177893245f4ae3eea4c80148b89c7cf2
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-10-19 22:17:20 -0400

    Creacion

---
+++ Service/TrabajosDetallesService.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq.Expressions;
+using RegistroTecnicos.DAL;
+using RegistroTecnicos.Models;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace RegistroTecnicos.Service
+{
+    public class TrabajosDetallesService
+    {
+        private readonly Context _context;
+
+        public TrabajosDetallesService(Context context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Trabajos>> Listar(Expression<Func<Trabajos, bool>> criterio)
+        {
+            return await _context.Trabajos
+                .AsNoTracking()
+                .Where(criterio)
+                .ToListAsync();
+        }
+    }
+}
+
+
