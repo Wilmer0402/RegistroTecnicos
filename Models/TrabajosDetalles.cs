1db38067cfcf60a49418fb99f8d52bd03748f969
Wilmer Miguel Castillo Tejada <wilmer_castillo@ucne.edu.do>
2024-10-19 22:17:51 -0400

    Creacion

---
+++ Models/TrabajosDetalles.cs
@@ -0,0 +1,42 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RegistroTecnicos.Models
+{
+    public class TrabajosDetalles
+    {
+        [Key]
+
+        public int DetallesId { get; set; }
+
+        [ForeignKey("Trabajos")]
+        public int TrabajoId { get; set; }
+
+        public Trabajos? Trabajos { get; set; }
+
+        [ForeignKey("Articulos")]
+
+        public int ArticulosId { get; set; }    
+
+        public Articulos? Articulos { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar la Cantidad ")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se Permiten Numeros")]
+
+        public int Cantidad { get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Precio ")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se Permiten Numeros")]
+
+        public double Precio {get; set; }
+
+        [Required(ErrorMessage = "Favor Ingresar el Costo ")]
+        [RegularExpression(@"^\d+(\.\d+)?$", ErrorMessage = "Solo se Permiten Numeros")]
+
+        public double Costo { get; set; }
+
+        public string? Descripcion { get; set; }
+
+
+    }
+}
